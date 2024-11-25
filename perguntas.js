90ab0a67c4f441ffffaa023d8ee47cc9ca476837
mafermunhozz <00001114822085SP@al.educacao.sp.gov.br>
2024-11-25 14:19:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 7 kg em g?',
+    '7kg em g é 7000 g'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2 elevado a 10?',
+    '2 elevado a 10 é 1024'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz melhor em Inglês?',
+    'Feio em inglês é better'
+)
